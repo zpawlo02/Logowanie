51791cba58268f3c0ff4d88aa482fd2c03850782
Rafał Hrabia <rafixese@gmail.com>
2018-11-03 19:10:23 +0100

    Added ParseServer config file

---
+++ app/src/main/java/com/catdev/dowork/Setup/StarterApplication.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.catdev.dowork.Setup;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+
+
+public class StarterApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Enable Local Datastore.
+    Parse.enableLocalDatastore(this);
+
+    // Add your initialization code here
+    Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+            .applicationId("XaS7kAiCGJibgAGtllJZidk290AvuONnWNiyuLvb")
+            .clientKey("jpvceHVIvm4Zk4CdzLC7Y2yawzb5TZWdvQW2HQlJ")
+            .server("https://parseapi.back4app.com/")
+            .build()
+    );
+
+
+
+
+    //ParseUser.enableAutomaticUser();
+
+    ParseACL defaultACL = new ParseACL();
+    defaultACL.setPublicReadAccess(true);
+    defaultACL.setPublicWriteAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+
+  }
+}
